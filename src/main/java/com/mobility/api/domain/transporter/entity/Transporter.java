e4bc418ca71c1f5d391e9811d7c6903a6688e489
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-11-03 18:30:14 +0900

    [#11] feat: transporter entity 코드

--- src/main/java/com/mobility/api/domain/transporter/entity/Transporter.java
+++ src/main/java/com/mobility/api/domain/transporter/entity/Transporter.java
@@ -1,4 +1,38 @@
 package com.mobility.api.domain.transporter.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transporters")
+@Getter
+@Setter
+@NoArgsConstructor // @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Transporter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20)
+    private String name;
+
+    private String phone;
+
+    private boolean isAutoDispatch;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "update_at")
+    private LocalDateTime updatedAt;
+
 }
