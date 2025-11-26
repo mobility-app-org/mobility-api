e3e49eec59b448537026f5ccc2aca3443a11592f
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-11-26 23:24:26 +0900

    [#12] feat: 비관락 timeout 설정

--- src/main/java/com/mobility/api/domain/dispatch/repository/DispatchRepository.java
+++ src/main/java/com/mobility/api/domain/dispatch/repository/DispatchRepository.java
@@ -2,10 +2,8 @@ package com.mobility.api.domain.dispatch.repository;
 
 import com.mobility.api.domain.dispatch.entity.Dispatch;
 import jakarta.persistence.LockModeType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
+import jakarta.persistence.QueryHint;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
@@ -14,7 +12,7 @@ public interface DispatchRepository extends JpaRepository<Dispatch, Long>,
         JpaSpecificationExecutor<Dispatch> { // 정렬, 필터 등을 위해 추가
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")}) // 3初
     @Query("SELECT d FROM Dispatch d WHERE d.id = :dispatchId")
     Optional<Dispatch> findByIdWithPessimisticLock(@Param("dispatchId") Long dispatchId);
-
 }
