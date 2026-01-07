af72fd89729229fbba31600b819591428d197543
jsgjsg <jsgcys0815@gmail.com>
2026-01-07 21:48:00 +0900

    [#47] docs: 기사 회원가입 및 로그인 요청 swagger 수정

--- src/main/java/com/mobility/api/domain/auth/dto/request/TransporterLoginReq.java
+++ src/main/java/com/mobility/api/domain/auth/dto/request/TransporterLoginReq.java
@@ -1,5 +1,9 @@
 package com.mobility.api.domain.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TransporterLoginReq(
+
+        @Schema(description = "기사 전화번호", example = "010-1234-1234")
         String phone
 ) {}
--- src/main/java/com/mobility/api/domain/auth/dto/request/TransporterSignupReq.java
+++ src/main/java/com/mobility/api/domain/auth/dto/request/TransporterSignupReq.java
@@ -1,7 +1,14 @@
 package com.mobility.api.domain.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TransporterSignupReq(
+        @Schema(description = "기사 이름", example = "김기사")
         String name,
+
+        @Schema(description = "기사 전화번호", example = "010-1234-1234")
         String phone,
+
+        @Schema(description = "자동배차 여부", example = "false")
         Boolean isAutoDispatch
 ) {}
