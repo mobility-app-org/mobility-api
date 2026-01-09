2b6ee144530ff23515d36d6dd5b7ea50bf97819d
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2026-01-09 15:54:42 +0900

    [#51] feat: BaseEntity 공통 엔티티 생성

---
+++ src/main/java/com/mobility/api/global/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.mobility.api.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+@NoArgsConstructor
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
